c9c93c5a025d6b7b955373e36fed566e903e0b0a
Drew Hadley <drewhadley.guitar@gmail.com>
2020-10-05 21:39:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,3 @@ colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(colors)
   puts ("R: #{aoc[0]}, O: #{aoc[1]}, Y: #{aoc[2]}, G: #{aoc[3]} B: #{aoc[4]}, I: #{aoc[5]}, V: #{aoc[6]}")
 end
-
