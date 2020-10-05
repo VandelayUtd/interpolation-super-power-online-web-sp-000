45039d18f3ba09ea65378aec4287d75b643a716b
Drew Hadley <drewhadley.guitar@gmail.com>
2020-10-05 21:39:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,7 @@
 # Write your #display_rainbow method here
-aoc = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-def display_rainbow(aoc)
+def display_rainbow(colors)
   puts ("R: #{aoc[0]}, O: #{aoc[1]}, Y: #{aoc[2]}, G: #{aoc[3]} B: #{aoc[4]}, I: #{aoc[5]}, V: #{aoc[6]}")
 end
 
-display_rainbow
