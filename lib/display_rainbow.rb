3ea65d7f4d4ae4b70f8554a2aec800365fca4ea0
Drew Hadley <drewhadley.guitar@gmail.com>
2020-10-05 21:45:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 
-def display_rainbow(aoc)
-  puts "R: #{aoc[0]}, O: #{aoc[1]}, Y: #{aoc[2]}, G: #{aoc[3]} B: #{aoc[4]}, I: #{aoc[5]}, V: #{aoc[6]}"
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]} B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
